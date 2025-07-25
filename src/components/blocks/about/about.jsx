cc169ed8574c89aa0f5d7448a757fbe2becfb92a
Vadim Tiuriumin <owlriks@gmail.com>
2025-07-25 13:35:33 +0000

    Стилизован блок About

--- src/components/blocks/about/about.jsx
+++ src/components/blocks/about/about.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import Title, {TitleSize} from "../../ui/title/title";
+import Title, { TitleSize } from "../../ui/title/title";
+import "./style.css";
 
-function About({level}) {
-    return (
-        <section className="about">
-            <div className="about__inner">
-                <div className="about__text-wrap">
-                    <Title level={level} size={TitleSize.BIG}>Магазин фермерских продуктов с&nbsp;доставкой</Title>
-                    <p className="about__text">
-                        Все продукты изготавливаются под заказ. Фермеры начинают готовить продукты за день до отправки
-                        заказа клиентам. Именно поэтому мы принимаем заказы заранее и доставляем продукты максимально
-                        свежими.
-                    </p>
-                </div>
-            </div>
-        </section>
-    );
+function About({ level }) {
+  return (
+    <section className="about">
+      <div className="about__inner">
+        <div className="about__text-wrap">
+          <Title level={level} size={TitleSize.BIG}>
+            Магазин фермерских продуктов с&nbsp;доставкой
+          </Title>
+          <p className="about__text">
+            Все продукты изготавливаются под заказ. Фермеры начинают готовить
+            продукты за день до отправки заказа клиентам. Именно поэтому мы
+            принимаем заказы заранее и доставляем продукты максимально свежими.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default About;
--- src/components/layout/page-wrapper/page-wrapper.jsx
+++ src/components/layout/page-wrapper/page-wrapper.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import MainPage from "../../pages/main-page";
+import "./style.css";
 
-function PageWrapper({...props}) {
-    return (
-        <>
-            <Header />
-            <main className="page-main">
-                <MainPage {...props} />
-            </main>
-            <Footer />
-        </>
-    );
+function PageWrapper({ ...props }) {
+  return (
+    <>
+      <Header />
+      <main className="page-main">
+        <MainPage {...props} />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default PageWrapper;
