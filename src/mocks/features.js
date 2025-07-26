263c6769be9d59bdbf1600750a1a468266748351
Vadim Tiuriumin <owlriks@gmail.com>
2025-07-26 14:14:39 +0000

    Исправляет пути к иконкам на сервере

--- src/mocks/features.js
+++ src/mocks/features.js
@@ -1,32 +1,36 @@
 const features = [
-    {
-        id: 0,
-        type: "farm",
-        title: "Еда намного вкуснее",
-        description: "Домашняя колбаса из&nbsp;мяса, соли и&nbsp;специй и&nbsp;колбаса из&nbsp;магазина&nbsp;— два&nbsp;настолько разных продукта, что&nbsp;они&nbsp;даже&nbsp;не&nbsp;родственники",
-        image: "https://ibb.co/7tXbHJbj",
-    },
-    {
-        id: 1,
-        type: "farm",
-        title: "Натуральные продукты",
-        description: "Поставляем местные органические продукты выращенные без пестицидов и химических удобрений.",
-        image: "https://ibb.co/QFCN7gXB",
-    },
-    {
-        id: 2,
-        type: "store",
-        title: "Просроченные продукты",
-        description: "Из-за&nbsp;большого количество товара сотрудники магазинов не&nbsp;успевают своевременно производить замену товара",
-        image: "https://ibb.co/Vpj22m5N",
-    },
-    {
-        id: 3,
-        type: "store",
-        title: "Некачественное мясо",
-        description: "Мясные полуфабрикаты, в&nbsp;которых содержится чрезмерно много натрия, вредных жиров, консервантов",
-        image: "https://ibb.co/spFjW9Fk",
-    },
+  {
+    id: 0,
+    type: "farm",
+    title: "Еда намного вкуснее",
+    description:
+      "Домашняя колбаса из&nbsp;мяса, соли и&nbsp;специй и&nbsp;колбаса из&nbsp;магазина&nbsp;— два&nbsp;настолько разных продукта, что&nbsp;они&nbsp;даже&nbsp;не&nbsp;родственники",
+    image: "https://i.ibb.co/5h62dW2G/feature-1-1.png",
+  },
+  {
+    id: 1,
+    type: "farm",
+    title: "Натуральные продукты",
+    description:
+      "Поставляем местные органические продукты выращенные без пестицидов и химических удобрений.",
+    image: "https://i.ibb.co/JRy7W1CL/feature-2-1.png",
+  },
+  {
+    id: 2,
+    type: "store",
+    title: "Просроченные продукты",
+    description:
+      "Из-за&nbsp;большого количество товара сотрудники магазинов не&nbsp;успевают своевременно производить замену товара",
+    image: "https://i.ibb.co/jZ6ggRs5/feature-3-1.png",
+  },
+  {
+    id: 3,
+    type: "store",
+    title: "Некачественное мясо",
+    description:
+      "Мясные полуфабрикаты, в&nbsp;которых содержится чрезмерно много натрия, вредных жиров, консервантов",
+    image: "https://i.ibb.co/1Y9ZzG9h/feature-4-1.png",
+  },
 ];
 
 export default features;
