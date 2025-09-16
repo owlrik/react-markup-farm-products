9daad4aaa9c7675ec5b92fed75312a70751a83f4
Vadim Tiuriumin <owlriks@gmail.com>
2025-09-16 12:02:59 +0000

    Исправляет скролл при переключении экранов

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,12 +7,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AppRoute } from "../utils/const";
 import MainPage from "../pages/main-page/main-page";
 import BuyPage from "../pages/buy-page/buy-page";
+import ScrollTop from "../ui/scroll-top/scroll-top";
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
       <Router>
+        <ScrollTop />
         <Routes>
           <Route path={AppRoute.MAIN} element={<PageWrapper />}>
             <Route index element={<MainPage features={features} />} />
